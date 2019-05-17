940fc55497c4a44c4192fba46eeafa146ec087c6
mathieu enault <mathieu.coding-academy.fr>
2019-05-17 19:06:33 +0200

    projet

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function scrollDown(){
-  window.scrollTo(0, document.body.scrollHeight);
+	var elmnt = document.getElementsByClassName("texte1");
+  elmnt.scrollIntoView();
+}
+function scrollDown1(){
+	var elmnt = document.getElementById("slide3");
+  elmnt.scrollIntoView();
+}
+function scrollDown2(){
+	var elmnt = document.getElementById("slide2");
+  elmnt.scrollIntoView();
+}
+function scrollDown3(){
+	var elmnt = document.getElementById("slide4");
+  elmnt.scrollIntoView();
 }
